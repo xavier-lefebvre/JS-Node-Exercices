1f9387ba4c259db629e0e52a25e9bc321064b489
xavier-lefebvre <xavierlefebvre60@gmail.com>
2022-01-04 16:41:36 +0100

    Second exercise finished

--- fiche-02/first-servers/routes.js
+++ fiche-02/first-servers/routes.js
@@ -10,6 +10,13 @@ const requestListener = function (req, res) {
             res.writeHead(200);
             res.end(books);
             break
+        case "/authors":
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}));
     }
 };
 
